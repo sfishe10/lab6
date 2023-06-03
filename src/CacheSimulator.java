5bd66424438aafcf9f3f925c6f7dd4f5e05f1c37
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-06-03 15:18:48 -0700

    Made code for printing out cache values and made cache array

--- src/CacheSimulator.java
+++ src/CacheSimulator.java
@@ -3,11 +3,26 @@ public class CacheSimulator {
     private int kb;
     private int association;
     private int wordblocks;
+    private int hits;
 
-    public CacheSimulator (int kb, int association, int wordblocks) {
+
+    public CacheSimulator (int kb, int association, int wordblocks, int hits) {
         this.kb = kb;
         // if association = 1, cache is direct mapped, otherwise set associative
         this.association = association;
         this.wordblocks = wordblocks;
+        this.hits = hits;
+    }
+    public int getkb() {
+        return this.kb;
+    }
+    public int getAssociation() {
+        return this.association;
+    }
+    public int getWordblocks() {
+        return this.wordblocks;
+    }
+    public int getHits() {
+        return this.hits;
     }
 }
--- src/lab6.java
+++ src/lab6.java
@@ -7,14 +7,33 @@ public class lab6 {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File(args[0]);
         LinkedHashMap<Integer, Integer> addresses = new LinkedHashMap<>();
+        CacheSimulator cache[] = new CacheSimulator[7];
+        int index = 0;
+        int tag = 0;
+        int count = 1;
+        int hit_rate = 0;
+        int size = 0;
 
-        CacheSimulator cache1 = new CacheSimulator(2, 1, 1);
-        CacheSimulator cache2 = new CacheSimulator(2, 1, 2);
-        CacheSimulator cache3 = new CacheSimulator(2, 1, 4);
-        CacheSimulator cache4 = new CacheSimulator(2, 2, 1);
-        CacheSimulator cache5 = new CacheSimulator(2, 4, 1);
-        CacheSimulator cache6 = new CacheSimulator(2, 4, 4);
-        CacheSimulator cache7 = new CacheSimulator(4, 1, 1);
+
+        CacheSimulator cache1 = new CacheSimulator(2, 1, 1, 0);
+        CacheSimulator cache2 = new CacheSimulator(2, 1, 2, 0);
+        CacheSimulator cache3 = new CacheSimulator(2, 1, 4, 0);
+        CacheSimulator cache4 = new CacheSimulator(2, 2, 1, 0);
+        CacheSimulator cache5 = new CacheSimulator(2, 4, 1, 0);
+        CacheSimulator cache6 = new CacheSimulator(2, 4, 4, 0);
+        CacheSimulator cache7 = new CacheSimulator(4, 1, 1, 0);
+
+        cache[0] = cache1;
+        cache[1] = cache2;
+        cache[2] = cache3;
+        cache[3] = cache4;
+        cache[4] = cache5;
+        cache[5] = cache6;
+        cache[6] = cache7;
+
+
+
+
 
         // LOGIC
         // read in address
@@ -28,10 +47,29 @@ public class lab6 {
         Scanner scnr = new Scanner(file);
         while (scnr.hasNext()) {
             String line = scnr.nextLine();
-            int address = Integer.decode(line);
+            int address = Integer.parseInt(line, 16);
             addresses.put(address, linenum);
             linenum++;
+
+            //cache 1
+           // index = address % 4;
+            tag = address >>>11;
+
         }
+
+        //Printing out
+        for (CacheSimulator item: cache) {
+            hit_rate = item.getHits()/linenum;
+            size = item.getkb()*1024;
+            System.out.println("Cache #" + count);
+            System.out.println("Cache size: " + size + "B" + "     Associativity: " + item.getAssociation()+ "     Block size: " + item.getWordblocks());
+            System.out.println("Hits: " + item.getHits() + "     Hit Rate: " + hit_rate + "%");
+            System.out.println("----------------------------");
+            count++;
+        }
+
+
+
 
 
     }
